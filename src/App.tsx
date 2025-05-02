d95f98d8187b10ef86061e670b2086d2880313d8
EhsaN Karimi <ehsan.karimmi@gmail.com>
2025-05-02 23:20:15 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div>
+      <Button type="primary">Button</Button>
+      <div className="bg-amber-400">hello</div>
+    </div>
+  );
+}
+
+export default App;
